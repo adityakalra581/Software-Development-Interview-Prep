a09187dc686d5b56b89f95cc1edbb6d5fb41f442
Aditya Kalra <51115390+adityakalra581@users.noreply.github.com>
2019-08-29 16:56:06 +0530

    Add files via upload
    IMP: This code will not run in IDLE till 3.7 . Try to use Jupyter notebook .

---
+++ Problems/calendar.py
@@ -0,0 +1,19 @@
+# Python program to display calendar of given month of the year
+
+# import module
+import calendar
+
+#yy = 2014
+#mm = 11
+
+# To ask month and year from the user
+yy = int(input("Enter year: "))
+mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
+
+
+
+### V.IMP : THIS WILL NOT RUN IN IDLE. IT WILL ONLY RUN ON JUPYTER NOTEBOOK.
+### IT WILL SHOW A MODULE ERROR.
