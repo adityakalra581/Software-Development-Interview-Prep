dc48b20d22597b44e24796c54454130a75dae586
Aditya Kalra <51115390+adityakalra581@users.noreply.github.com>
2019-09-09 14:35:52 +0530

    Basic Series Sum
     Sum of series= 1+(1+2)+(1+2+3)+…+(1+2+3+…+n)

---
+++ Problems/sum of  series.py
@@ -0,0 +1,16 @@
+#18. WAP to find the sum of the given series till n terms
+#1+(1+2)+(1+2+3)+…+(1+2+3+…+n)
+
+
+
+
+
+n=int(input())
+sum1=0
+a=[]
+for i in range(1,n+1):
+    sum1=sum1+i
+    a.append(sum1)
+#print(sum1)
+print(a)
+print("The Sum Of the Series is: ",sum(a))
