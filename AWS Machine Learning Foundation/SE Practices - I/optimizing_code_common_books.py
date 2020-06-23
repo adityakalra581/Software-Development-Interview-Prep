a1b896b78b4c260c353a0aa6fa8fdb70901a7946
Aditya Kalra <51115390+adityakalra581@users.noreply.github.com>
2020-06-23 20:00:24 +0700

    Update

--- AWS Machine Learning Foundation/SE Practices - I/optimizing_code_common_books.py
+++ AWS Machine Learning Foundation/SE Practices - I/optimizing_code_common_books.py
@@ -2,7 +2,8 @@
 # coding: utf-8
 
 # # Optimizing Code: Common Books
-# Here's the code your coworker wrote to find the common book ids in `books_published_last_two_years.txt` and `all_coding_books.txt` to obtain a list of recent coding books.
+# Here's the code your coworker wrote to find the common book ids 
+# in `books_published_last_two_years.txt` and `all_coding_books.txt` to obtain a list of recent coding books.
 
 # In[1]:
 
@@ -35,6 +36,10 @@ for book in recent_books:
 print(len(recent_coding_books))
 print('Duration: {} seconds'.format(time.time() - start))
 
+## Output:
+# 96
+# Duration: 16.786165237426758 seconds
+
 
 # ### Tip #1: Use vector operations over loops when possible
 # 
@@ -48,6 +53,13 @@ recent_coding_books =  np.intersect1d(coding_books, recent_books) # TODO: comput
 print(len(recent_coding_books))
 print('Duration: {} seconds'.format(time.time() - start))
 
+## Output:
+# 96
+# Duration: 0.03507089614868164 seconds
+
+
+
+## SETS:
 
 # ### Tip #2: Know your data structures and which methods are faster
 # Use the set's `intersection` method to get the common elements in `recent_books` and `coding_books`.
@@ -63,9 +75,12 @@ recent_coding_books =  set1.intersection(set2) # TODO: compute intersection of l
 print(len(recent_coding_books))
 print('Duration: {} seconds'.format(time.time() - start))
 
+## Output:
+# 96
+# Duration: 0.009238481521606445 seconds
 
 # In[ ]:
 
-
+## Conclusion: Sets approach is faster and next is numpy as compared to traditional loop method.
 
 
